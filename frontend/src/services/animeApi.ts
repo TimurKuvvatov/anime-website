6aacb10a2dbc51b0817e36d46b7fdf8ebd468e60
TimurKuvvatov <tkuvvatov@gmail.com>
2025-02-02 12:51:29 +0600

    Add the method getAnimeRelations to fetch related anime

--- frontend/src/services/animeApi.ts
+++ frontend/src/services/animeApi.ts
@@ -1,18 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { IAnimeCart } from '../models/IAnimeCart';
 import { IAnime } from '../models/IAnime';
+import { IAnimeRelations } from '../models/IAnimeRelation';
 
 export const animeApi = createApi({
     reducerPath: 'animeApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://shikimori.one/api/animes' }),
     endpoints: (builder) => ({
-        getAllAnimes: builder.query<IAnimeCart[], { limit: number; search?: string, order?: string }>({
-            query: ({ limit, search, order }) => ({
+        getAllAnimes: builder.query<
+            IAnimeCart[],
+            { limit: number; search?: string; order?: string; page?: number; genre?: number[] }
+        >({
+            query: ({ limit, search, order, page, genre }) => ({
                 url: '',
                 params: {
                     limit,
                     order,
                     search,
+                    page,
+                    genre,
                 },
             }),
         }),
@@ -21,5 +27,12 @@ export const animeApi = createApi({
                 url: `/${id}`,
             }),
         }),
+        getAnimeRelations: builder.query<IAnimeRelations[], number>({
+            query: (id) => ({
+                url: `/${id}/related`,
+            }),
+        }),
     }),
 });
+
+export const { useGetAllAnimesQuery } = animeApi;
