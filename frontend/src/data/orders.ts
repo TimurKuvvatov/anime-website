f5dd6cbae8bc706423dbf4144934a33b5f9a5c3e
TimurKuvvatov <tkuvvatov@gmail.com>
2025-02-07 23:07:34 +0600

    Add datasets for genres, kinds, orders and anime statuses to enable filtering and sorting anime listings

---
+++ frontend/src/data/orders.ts
@@ -0,0 +1,26 @@
+export const orders = [
+    {
+        name: 'popularity',
+        russian: 'Популярные',
+    },
+    {
+        name: 'ranked',
+        russian: 'С высоким рейтингом',
+    },
+    {
+        name: 'aired_on',
+        russian: 'Новинки',
+    },
+    {
+        name: 'name',
+        russian: 'По алфавиту',
+    },
+    {
+        name: 'random',
+        russian: 'Вразброс',
+    },
+    {
+        name: 'episodes',
+        russian: 'По количеству эпизодов',
+    },
+];
