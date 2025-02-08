32607531501f632d3012204c13ac6b5d786664b5
TimurKuvvatov <tkuvvatov@gmail.com>
2025-02-09 00:47:48 +0600

    Add Orders component

--- frontend/src/components/Filter/Filter.tsx
+++ frontend/src/components/Filter/Filter.tsx
@@ -2,14 +2,17 @@ import styles from './Filter.module.scss';
 
 import ViewModeList from '../ViewModeList/ViewModeList';
 import GenresList from '../GenresList/GenresList';
+import Orders from '../Orders/Orders';
 
 const Filter = () => {
     return (
         <div className={styles.filter}>
-            <h3>Жанры</h3>
-            <GenresList />
+            <h3>Сортировка</h3>
+            <Orders />
             <h3>Отображение</h3>
             <ViewModeList />
+            <h3>Жанры</h3>
+            <GenresList />
         </div>
     );
 };
