54dabffe0bda22314013a550a347da532006772e
TimurKuvvatov <tkuvvatov@gmail.com>
2025-02-11 00:42:36 +0600

    Create WelcomeSection component to main page

---
+++ frontend/src/components/WelcomeSection/WelcomeSection.tsx
@@ -0,0 +1,11 @@
+import styles from './WelcomeSection.module.scss';
+
+const WelcomeSection = () => {
+  return (
+    <section className={styles.section}>
+        
+    </section>
+  )
+}
+
+export default WelcomeSection
