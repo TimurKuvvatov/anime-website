c31671f7c505caf9bc9b1490658285103addcd7e
TimurKuvvatov <tkuvvatov@gmail.com>
2025-02-13 22:28:45 +0600

    Implement search parameters handling logic in anime list page

--- frontend/src/pages/AnimeListPage.tsx
+++ frontend/src/pages/AnimeListPage.tsx
@@ -1,12 +1,25 @@
-import { FC, useEffect, useRef, useState } from 'react';
-import AnimeList from '../components/AnimeList/AnimeList';
+import { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { animeApi } from '../services/animeApi';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { setPage } from '../redux/slices/pageSlice';
+
 import { IAnimeCard } from '../models/IAnimeCard';
+import { IGenre } from '../models/IGenre';
+
+import AnimeList from '../components/AnimeList/AnimeList';
+import { setOrder } from '../redux/slices/orderSlice';
+import { setSearch } from '../redux/slices/searchSlice';
+import { toggleKinds } from '../redux/slices/kindsSlice';
+import { IKind } from '../models/IKind';
+import { toggleGenre } from '../redux/slices/genreSlice';
+import { genres as dataGenres } from '../data/genres';
 
 const AnimeListPage: FC = () => {
+    const navigate = useNavigate();
     const dispatch = useAppDispatch();
+    const [searchParams, setSearchParams] = useSearchParams();
+
     const search = useAppSelector((state) => state.search.value);
     const page = useAppSelector((state) => state.page.value);
 
@@ -22,6 +35,26 @@ const AnimeListPage: FC = () => {
     const prevGenresId = useRef<number>(genresId.length);
 
     const [hasMore, setHasMore] = useState<boolean>(true);
+
+    const buildAnimeUrl = useCallback(
+        (order: string, genres: IGenre[], kind: string[], search: string) => {
+            const params = new URLSearchParams();
+            if (order) params.append('order', order);
+            if (search) params.append('search', search);
+            if (genres && genres.length > 0) {
+                genres.forEach((genre) => params.append('genre', genre.name));
+            }
+            if (kinds && kinds.length > 0) {
+                kinds.forEach((kind) => params.append('kind', kind.name));
+            }
+            return `?${params.toString()}`;
+        },
+        [order, genres, kinds, search],
+    );
+
+    const apiUrl = useMemo(() => {
+        return buildAnimeUrl(order, genres, kindsNames, search);
+    }, [order, genres, kindsNames, search]);
 
     const {
         data: animes = [],
@@ -45,6 +78,39 @@ const AnimeListPage: FC = () => {
 
     const [initialAnimes, setInitialAnimes] = useState<IAnimeCard[]>([]);
 
+    const paramsFromUrl = useCallback(() => {
+        const orderFromUrl = searchParams.get('order') || 'popularity';
+        const genreFromUrl = searchParams.getAll('genre') || '';
+        const kindFromUrl = searchParams.getAll('kind') || '';
+        const searchFromUrl = searchParams.get('search') || '';
+
+        if (orderFromUrl !== order) {
+            dispatch(setOrder(orderFromUrl));
+        }
+        if (searchFromUrl !== search) {
+            dispatch(setSearch(searchFromUrl));
+        }
+        const kindsFromUrl: IKind[] = kindFromUrl
+            .map((name) => {
+                const kind = kinds.find((k) => k.name === name);
+                return kind ? kind : { name: name, russian: '' };
+            })
+            .filter((kind) => kind !== undefined);
+
+        kindsFromUrl.forEach((kind) => dispatch(toggleKinds(kind)));
+        const genresFromUrl = genreFromUrl
+            .map((name) => {
+                const genre = dataGenres.findIndex((genreEl) => genreEl.name === name);
+                return dataGenres[genre];
+            })
+            .filter((genre) => genre !== undefined);
+        genresFromUrl.forEach((genre) => dispatch(toggleGenre(genre)));
+    }, [order, search, kinds, genres]);
+
+    useEffect(() => {
+        paramsFromUrl();
+    }, []);
+
     useEffect(() => {
         if (
             prevGenresId.current !== genresId.length ||
@@ -60,6 +126,7 @@ const AnimeListPage: FC = () => {
             prevGenresId.current = genresId.length;
             prevKindNames.current = kindsNames.length;
             prevOrder.current = order;
+            navigate(apiUrl, { replace: true });
         }
     }, [genresId, isFetching, page, order, kindsNames, dispatch]);
 
@@ -67,6 +134,7 @@ const AnimeListPage: FC = () => {
         if (animes.length > 0 && !isFetching) {
             setInitialAnimes((prev) => (page > 1 ? [...prev, ...animes] : animes));
             console.log('Лист аниме подгружается');
+
             setHasMore(true);
         } else if (animes.length === 0 && isSuccess) {
             setHasMore(false);
